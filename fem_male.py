14a1fca36a780ba1529c95c72d6f87d35013ca70
aidamoo <aida.amoedo.port@gmail.com>
2024-02-19 04:30:39 +0100

    18 commit

--- fem_male.py
+++ fem_male.py
@@ -1,18 +1,17 @@
 import streamlit as st
 from PIL import Image
-import numpy as np
-import pandas as pd
-import requests
-import os
-import pickle
-import matplotlib.pyplot as plt
-import random
-import matplotlib.image as mpimg
-from keras.preprocessing import image
 from modelo_page import modelo_page
 from final import final
 from intro import intro
 from m_imagenes import m_imagenes
+import numpy as np
+import pandas as pd
+import requests
+import os
+import matplotlib.pyplot as plt
+import random
+import matplotlib.image as mpimg
+from keras.preprocessing import image
 
 
 
