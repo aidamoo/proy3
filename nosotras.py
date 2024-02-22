b9cc1344769aaa6255e9c661e2448329afb634e1
aidamoo <160170012+aidamoo@users.noreply.github.com>
2024-02-22 20:44:52 +0100

    Update nosotras.py

--- nosotras.py
+++ nosotras.py
@@ -3,9 +3,10 @@ from PIL import Image
 
 def nosotras():
     st.title("Nuestro equipo")
-    st.write("**Esther García **")
+    st.write("**Esther García**")
     image = Image.open("imagenes/esther.jpeg")
     st.image(image=image, width=200)
+    st.text("")
     st.text("")
 
 
@@ -13,12 +14,14 @@ def nosotras():
     image2 = Image.open("imagenes/bea.jpeg")
     st.image(image=image2, width=200)
     st.text("")
+    st.text("")
 
 
     st.write("**Aida Amoedo**")
     st.write("**Linkedin: http://linkedin.com/in/aida-amoedo**")
     image3 = Image.open("imagenes/aida.jpeg")
     st.image(image=image3, width=200)
+    st.text("")
     st.text("")
 
 if __name__ == "__main__":
