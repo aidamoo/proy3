758442fcbbd54e5a0c84490b0a0a5703f31bd750
aidamoo <160170012+aidamoo@users.noreply.github.com>
2024-02-22 20:41:12 +0100

    Update nosotras.py

--- nosotras.py
+++ nosotras.py
@@ -5,22 +5,19 @@ def nosotras():
     st.title("Nuestro equipo")
     st.write("** Esther García **")
     image = Image.open("imagenes/esther.jpeg")
-    resized_image_esther = image.resize((100, 100)) 
-    st.image(resized_image_esther, caption="Esther García", use_column_width=True)
+    st.image(image=image, width=200)
     st.text("")
 
 
     st.write("**Beatriz Mimosa** ")
     image2 = Image.open("imagenes/bea.jpeg")
-    resized_image_bea = image2.resize((100, 100)) 
-    st.image(resized_image_bea, caption="Beatriz Mimosa", use_column_width=True)
+    st.image(image=image2, width=200)
     st.text("")
 
 
     st.write("**Aida Amoedo**")
     image3 = Image.open("imagenes/aida.jpeg")
-    resized_image_aida = image3.resize((100, 100)) 
-    st.image(resized_image_aida, caption="Aida Amoedo", use_column_width=True)
+    st.image(image=image3, width=200)
     st.text("")
 
 if __name__ == "__main__":
