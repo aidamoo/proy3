694577e645744011babc3806ce459e16291359bd
aidamoo <160170012+aidamoo@users.noreply.github.com>
2024-02-22 20:34:41 +0100

    Update nosotras.py

--- nosotras.py
+++ nosotras.py
@@ -4,19 +4,19 @@ from PIL import Image
 def nosotras():
     st.title("Nuestro equipo")
     st.write("** Esther García **")
-    image = Image.open("/home/hacker/PycharmProjects/proy3/imagenes/esther.jpeg")
+    image = Image.open("imagenes/esther.jpeg")
     st.image(image, caption="Esther García", use_column_width=True)
     st.text("")
 
 
     st.write("**Beatriz Mimosa** ")
-    image2 = Image.open("/home/hacker/PycharmProjects/proy3/imagenes/bea.jpeg")
+    image2 = Image.open("imagenes/bea.jpeg")
     st.image(image2, caption="Beatriz Mimosa", use_column_width=True)
     st.text("")
 
 
     st.write("**Aida Amoedo**")
-    image3 = Image.open("/home/hacker/PycharmProjects/proy3/imagenes/aida.jpeg")
+    image3 = Image.open("imagenes/aida.jpeg")
     st.image(image3, caption="Aida Amoedo", use_column_width=True)
     st.text("")
 
