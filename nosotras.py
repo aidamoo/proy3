b9e2cd9e6ffb7c2318baea9703bba4f81d7acfa1
aidamoo <160170012+aidamoo@users.noreply.github.com>
2024-02-22 20:59:19 +0100

    Update nosotras.py

--- nosotras.py
+++ nosotras.py
@@ -3,28 +3,32 @@ from PIL import Image
 
 def nosotras():
     st.title("Nuestro equipo")
-    st.write("**Esther García**")
-    st.write("**Linkedin: http://linkedin.com/in/esthergarciagonzalezgg**")
-    image = Image.open("imagenes/esther.jpeg")
-    st.image(image=image, width=200)
-    st.text("")
-    st.text("")
 
+    col1, col2, col3 = st.beta_columns(3)
 
-    st.write("**Beatriz Mimosa**")
-    st.write("**Linkedin: http://linkedin.com/in/beatriz-mimosa**")
-    image2 = Image.open("imagenes/bea.jpeg")
-    st.image(image=image2, width=200)
-    st.text("")
-    st.text("")
+    with col1:
+        st.write("**Esther García**")
+        st.write("**Linkedin: http://linkedin.com/in/esthergarciagonzalezgg**")
+        image = Image.open("imagenes/esther.jpeg")
+        st.image(image=image, width=200)
+        st.text("")
+        st.text("")
 
+    with col2:
+        st.write("**Beatriz Mimosa**")
+        st.write("**Linkedin: http://linkedin.com/in/beatriz-mimosa**")
+        image2 = Image.open("imagenes/bea.jpeg")
+        st.image(image=image2, width=200)
+        st.text("")
+        st.text("")
 
-    st.write("**Aida Amoedo**")
-    st.write("**Linkedin: http://linkedin.com/in/aida-amoedo**")
-    image3 = Image.open("imagenes/aida.jpeg")
-    st.image(image=image3, width=200)
-    st.text("")
-    st.text("")
+    with col3:
+        st.write("**Aida Amoedo**")
+        st.write("**Linkedin: http://linkedin.com/in/aida-amoedo**")
+        image3 = Image.open("imagenes/aida.jpeg")
+        st.image(image=image3, width=200)
+        st.text("")
+        st.text("")
 
 if __name__ == "__main__":
     nosotras()
